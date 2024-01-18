2470d3730cec7e68e3c7e3fc694209bf5929eadd
Hindol19 <hindolbanerjee5@gmail.com>
2024-01-18 22:20:24 +0530

    Fixed ADX Indicator Code (Concatenated ADX column to initial data farmes)

--- adx/adx2.py
+++ adx/adx2.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 pd.options.mode.chained_assignment = None
 
 # stock_data = pd.read_csv('data/ADANIGREEN.NS.csv')
@@ -49,9 +50,9 @@ def calc_tr(df, index):
 
 def calc_first_14(df, index, column):
     result = 0
-    for i in range(index - 13, index+1):
-        result += df.loc[index, 'TR']
-    return result
+    for i in range(index-13, index+1):
+        result += df.loc[i, column]
+    return (result)
 
 
 def calc_sub_14(df, index, column):
@@ -60,70 +61,81 @@ def calc_sub_14(df, index, column):
 
 def calc_first_adx(df, index):
     result = 0
-    for i in range(index - 13, index+1):
+    for i in range(index-13, index+1):
         result += df.loc[i, 'DX']
     return (result/14)
 
 
 def calc_adx(df, index):
-    return (((df.loc[index-1, 'ADX']*13) + df.loc[index, 'DX'])/14)
+    return ((df.loc[index-1, 'ADX']*13) + df.loc[index, 'DX'])/14
 
 
 # df = stock_data
 
 
-def func(df):
-    for i in range(1, len(df)):
-        dm_pos, dm_neg = calc_dm(df, i)
-        tr = calc_tr(df, i)
-        df.loc[i, '+DM'] = dm_pos
-        df.loc[i, '-DM'] = dm_neg
-        df.loc[i, 'TR'] = tr
+def func(stock_data):
+    for i in range(1, len(stock_data)):
+        dm_pos, dm_neg = calc_dm(stock_data, i)
+        TR = calc_tr(stock_data, i)
+        stock_data.loc[i, '+DM'] = dm_pos
+        stock_data.loc[i, '-DM'] = dm_neg
+        stock_data.loc[i, 'TR'] = TR
 
-        if df.TR.count() == 14:
-            # Does'nt count NaN values
-            df.loc[i, 'TR14'] = calc_first_14(df, i, 'TR')
-            df.loc[i, '+DM14'] = calc_first_14(df, i, '+DM')
-            df.loc[i, '-DM14'] = calc_first_14(df, i, '-DM')
+        if stock_data.TR.count() == 14:
+            stock_data.loc[i, 'TR14'] = calc_first_14(stock_data, i, 'TR')
+            stock_data.loc[i, '+DM14'] = calc_first_14(stock_data, i, '+DM')
+            stock_data.loc[i, '-DM14'] = calc_first_14(stock_data, i, '-DM')
 
-        elif df.TR.count() >= 14:
-            # Does'nt count NaN values
-            df.loc[i, 'TR14'] = calc_sub_14(df, i, 'TR')
-            df.loc[i, '+DM14'] = calc_sub_14(df, i, '+DM')
-            df.loc[i, '-DM14'] = calc_sub_14(df, i, '-DM')
+        elif stock_data.TR.count() >= 14:
+            stock_data.loc[i, 'TR14'] = round(
+                calc_sub_14(stock_data, i, 'TR'), 2)
+            stock_data.loc[i,
+                           '+DM14'] = calc_sub_14(stock_data, i, '+DM')
+            stock_data.loc[i,
+                           '-DM14'] = calc_sub_14(stock_data, i, '-DM')
 
-        if 'TR14' in df.columns:
-            df.loc[i, '+DI'] = (df.loc[i,
-                                       '+DM14']/df.loc[i, 'TR14'])*100
-            df.loc[i, '-DI'] = (df.loc[i,
-                                       '-DM14']/df.loc[i, 'TR14'])*100
-            df.loc[i, 'DX'] = (
-                abs(df.loc[i, '+DI']-df.loc[i, '-DI'])/abs(df.loc[i, '+DI']+df.loc[i, '-DI']))*100
+        if 'TR14' in stock_data.columns:
+            stock_data.loc[i, '+DI'] = (stock_data.loc[i,
+                                        '+DM14'] / stock_data.loc[i, 'TR14'])*100
+            stock_data.loc[i, '-DI'] = (stock_data.loc[i,
+                                        '-DM14'] / stock_data.loc[i, 'TR14'])*100
 
-        if 'DX' in df.columns:
-            if df.DX.count() == 14:
-                df.loc[i, 'ADX'] = calc_first_adx(df, i)
+            den = abs(
+                stock_data.loc[i, '+DI'] + stock_data.loc[i, '-DI'])
+            num = abs(stock_data.loc[i, '+DI'] -
+                      stock_data.loc[i, '-DI'])
+            if den != 0 and not np.isnan(den):
+                stock_data.loc[i, 'DX'] = (num/den)*100
 
-            elif df.DX.count() >= 14:
-                df.loc[i, 'ADX'] = calc_adx(df, i)
+        if 'DX' in stock_data.columns:
+            if stock_data.DX.count() == 14:
+                stock_data.loc[i, 'ADX'] = calc_first_adx(stock_data, i)
+
+            elif stock_data.DX.count() >= 14:
+                stock_data.loc[i, 'ADX'] = calc_adx(stock_data, i)
 
 
 folder_path = 'data'
 file_list = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
 for file_name in file_list:
-    stock_data = pd.read_csv(folder_path+'/'+file_name)
+    sd = pd.read_csv(folder_path+'/'+file_name)
     high, low, close = 'High', 'Low', 'Close'
     columns = ['Date', high, low, close]
-    stock_data = stock_data[columns]
+    stock_data = sd[columns]
 
     func(stock_data)
 
     if 'ADX' in stock_data.columns:
-        stock_data = stock_data.dropna(subset=['ADX'])
+        sd['ADX'] = stock_data[['ADX']]
+        sd.to_csv(f"data/{file_name}", index=False)
+        print("Done")
     else:
-        print("Column 'adx' not found in the DataFrame.")
-    stock_data.reset_index(drop=True, inplace=True)
-
-    if stock_data.ADX.count() >= 1:
-        stock_data[['Date', 'ADX']].to_csv(f'adx\DATA_CALCULATED\{file_name}')
+        print("ADX does'nt exist")
+    # if 'ADX' in stock_data.columns:
+    #     stock_data = stock_data.dropna(subset=['ADX'])
+    # else:
+    #     print("Column 'adx' not found in the DataFrame.")
+    # stock_data.reset_index(drop=True, inplace=True)
+    # if 'ADX' in stock_data.columns:
+    #     stock_data[['Date', 'ADX']].to_csv(f'adx\DATA_CALCULATED\{file_name}')
