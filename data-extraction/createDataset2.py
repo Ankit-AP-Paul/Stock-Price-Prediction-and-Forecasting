f9d48d5a60dbaa897bb17084b077e3c815b2196a
Ankit Paul <ankitpaul894@gmail.com>
2024-03-30 16:54:27 +0530

    added company X indicators data set

--- data-extraction/createDataset2.py
+++ data-extraction/createDataset2.py
@@ -10,9 +10,23 @@ from stoch.stoch import calculate_stoch
 from VWAP.VWAP import calculate_vwap
 
 
+def calculate_output(profit):
+    if 0 <= profit <= 0.5:
+        return 3
+    elif 0.5 < profit < 2:
+        return 4
+    elif profit > 2:
+        return 5
+    elif -2 < profit < 0:
+        return 2
+    elif profit < -2:
+        return 1
+
+
 def createDataset(stock_ticker):
     data = yf.download(tickers=stock_ticker, period='1y',
                        prepost=True, actions=True)
+    # data.insert(4, 'Prev Adj Close', data['Adj Close'].shift(1))
     data['RSI'] = calc_rsi(data)
     data["SMA"] = calculate_sma(data.Close, window_size=100)
     data['13MA'] = calculate_sma(data.Close, window_size=13)
@@ -24,11 +38,15 @@ def createDataset(stock_ticker):
     calculate_bbands(data)
     calculate_macd(data)
     calculate_stoch(data)
+    data['Profit'] = ((data['Adj Close'] -
+                      data['Adj Close'].shift(1)) / data['Adj Close'].shift(1)) * 100
+    data['Output'] = data['Profit'].apply(calculate_output)
 
     df1 = data.tail(1)
     df1 = df1.reset_index()
     df1 = df1.drop('Date', axis=1)
     df1.insert(0, 'Stock', stock_ticker)
+
     # print(df1)
     return df1
 
@@ -38,4 +56,5 @@ df = pd.DataFrame()
 for ticker in file:
     df1 = createDataset(ticker[:-1])
     df = pd.concat([df, df1], axis=0, ignore_index=True)
-print(df)
+# print(df)
+pd.DataFrame(df).to_csv('models\dataset.csv', index=False)
