d3610e220c2ce3947f74b4b40af1b90698ba6f8c
AnkitInfomaticae <ankit.paul@infomaticae.co.in>
2025-04-24 21:56:23 +0530

    Revert "added code for stock data generation and retrival"
    This reverts commit 220f3d0cde5e3092c80ffda9c617eb437d4ef1c8.

--- data-generation-ms/src/main.py
+++ data-generation-ms/src/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.generate_stock_data import generate_stock_data
-from src.retrive_stock_data import retrive_stock_data
 
 app = FastAPI()
 
@@ -19,30 +17,3 @@ app.add_middleware(
 @app.get("/")
 def health_check():
     return {"message": "Data Generation microservice is running!", "status": 200}
-
-
-@app.get("/generate-data", tags=["Data Generation"])
-def generate_data():
-    try:
-        file = open('src/tickers.txt', 'r')
-        for ticker in file:
-            ticker = ticker.strip()
-            if ticker:
-                generate_stock_data(ticker)
-        return {"message": "Data generation completed successfully", "status": 200}
-    except Exception as e:
-        return {"message": f"Error generating stock data: {str(e)}", "status": 500}
-    finally:
-        file.close()
-
-
-@app.get("/retrive-data", tags=["Data Retrieval"])
-def retrive_data(ticker: str):
-    try:
-        data = retrive_stock_data(ticker)
-        for col in data.select_dtypes(include=['datetime64']).columns:
-            data[col] = data[col].dt.strftime('%Y-%m-%d')
-        data_dict = data.to_dict(orient="records")
-        return {"data": data_dict, "message": "Stock data retrieved successfully", "status": 200}
-    except Exception as e:
-        return {"message": f"Error retrieving stock data: {str(e)}", "status": 500}
